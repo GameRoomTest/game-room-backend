4c1b509371f8cf05d9ccaa569db6d1ecf61bb9d0
Joel Flores <joelf.-@live.com>
2024-03-01 00:59:49 -0300

    Create createGame and joinGame events

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import { Server } from 'socket.io';
 
 // Own
 import { PORT } from './constants';
+import { Event } from './socket/tateti/types';
+// import { Data as CreateGameData } from './socket/tateti/create-game';
 
 // Create server
 const app = express();
@@ -20,14 +22,106 @@ server.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
 
+export interface Player {
+  id: string;
+  name: string;
+  socketId: string;
+}
+
+type Code = string;
+
+export interface Room {
+  code: Code;
+  players: Player[];
+}
+
+const rooms: Record<Code, Room> = {}
+
 io.on('connection', (socket) => {
   console.log('a user connected');
   
   socket.on('disconnect', () => {
-    console.log('user disconnected');
+    const userDisconnectedSocketId = socket.id;
+
+    const room = Object.values(rooms)
+      .find(r => r.players.some(p => p.socketId === userDisconnectedSocketId))
+    
+    if(!room) return;
+
+    const roomCode = room.code;
+
+    const playerTwo = room.players.filter(x => x.socketId !== userDisconnectedSocketId)[0];
+    
+    // Eliminamos la sala del usuario que se desconecto
+    delete rooms[roomCode]
+
+    // Echar al otro usuario de la sala
+    io.sockets.sockets.forEach(x => {
+      if(x.id === playerTwo.socketId) {
+        x.disconnect()
+      }
+    })
+
+    console.log('room deleted', roomCode);
   });
 
-  socket.on('message', (data) => {
-    console.log(data);
-  });
+  socket.on(Event.CREATE_GAME, (data: Pick<Player, 'id' | 'name'>) => {
+    const roomCode = createRoom(data, socket.id);
+    
+    socket.emit(Event.GAME_CREATED, roomCode)
+  })
+
+  socket.on(Event.JOIN_GAME, (data: Player & {code: Code}) => {
+    const {code, ...player} = data;
+
+    const joinStatus = joinRoom(code, player, socket.id);
+
+    if(joinStatus) {
+      const room = rooms[code];
+
+      socket.emit(Event.JOINED_IN_GAME, room)
+      console.log(JSON.stringify(rooms))
+    } else {
+      socket.emit(Event.ERROR_JOINING)
+    }
+  })
 });
+
+function joinRoom(code: Code, player: Player, socketId: string): boolean {
+  const room = rooms[code];
+
+  if(room && room.players.length === 1) {
+    const currentPlayer = rooms[code].players[0];
+
+    if(currentPlayer.id !== player.id) {
+      rooms[code].players.push({...player, socketId});
+      return true
+    }
+  }
+
+  return false
+}
+
+function createRoom(player: Pick<Player, 'id' | 'name'>, socketId: string) {
+  const code = generateRandomCode();
+
+  rooms[code] = {
+    code,
+    players: [{...player, socketId}]
+  }
+
+  return code;
+}
+
+function generateRandomCode(): string {
+  const chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const length = 6;
+
+  let result = '';
+
+  for (let i = length; i > 0; --i) {
+    result += chars[Math.floor(Math.random() * chars.length)];
+  }
+
+  return result;
+}
--- src/test.test.ts
+++ src/test.test.ts
@@ -1,16 +1,64 @@
 import { createServer } from "node:http";
 import { io as ioc, type Socket as ClientSocket } from "socket.io-client";
 import { PORT } from "./constants";
+import { Event } from "./socket/tateti/types";
+import { Player, Room } from ".";
 
 const httpServer = createServer();
 let clientSocket: ClientSocket;
+let clientSocket2: ClientSocket;
 
 httpServer.listen(() => {
   clientSocket = ioc(`http://localhost:${PORT}`);
+  clientSocket2 = ioc(`http://localhost:${PORT}`);
 
   clientSocket.on("connect", () => {
-    console.log('I am a client');
+    console.log('I am the player ONE');
   });
 
-  clientSocket.emit('message', {foo: 123})
+  clientSocket.on("disconnect", () => {
+    console.log('Player ONE disconnected');
+  });
+
+  const player = {
+    id: '123qwe',
+    name: 'Dani',
+  }
+
+  clientSocket.emit(Event.CREATE_GAME, player)
+
+  clientSocket.on(Event.GAME_CREATED, (roomCode: string) => {
+    // mostrar codigo en la UI
+    console.log(roomCode)
+
+    joinPlayerTwo(roomCode)
+  })
+
+  function joinPlayerTwo(code: string) {
+    const joinGameData = {
+      id: 'zxc789',
+      name: 'Karen',
+    }
+
+    clientSocket2.on("connect", () => {
+      console.log('I am the player TWO');
+    });
+
+    clientSocket2.on("disconnect", () => {
+      console.log('Player TWO disconnected');
+    });
+
+    clientSocket2.emit(Event.JOIN_GAME, { ...joinGameData, code })
+    
+    clientSocket2.on(Event.JOINED_IN_GAME, (room: Room) => {
+      console.log(room)
+
+      clientSocket2.disconnect();
+    })
+    
+    clientSocket2.on(Event.ERROR_JOINING, () => {
+      console.log('Error al unirse al juego')
+    })
+  }
+    
 });
